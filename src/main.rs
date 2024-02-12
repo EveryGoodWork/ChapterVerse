0e9d314eeb1b9807bcdb80a3fe350bde8bc738c4
Peter Hammond <peter@everygoodwork.online>
2024-02-12 15:24:59 -0800

    Retrieves the path to the build location and copies the bible.csv files to that destination.
    These initial bibles are copyright free.  More bibles will be introduced however will not be checked in to avoid copyright issues.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Jesus is Lord!");
+    println!("Jesus is Lord!!");
+    println!("Gospel means good news! The bad news is we have all sinned and deserve the wrath to come. But Jesus the Messiah died for our sins, was buried, and then raised on the third day, according to the scriptures. He ascended into heaven and right now is seated at the Father's right hand. Jesus said, \"I am the way, and the truth, and the life. No one comes to the Father except through me. The time is fulfilled, and the kingdom of God is at hand; repent and believe in the gospel.\"");    
 }
