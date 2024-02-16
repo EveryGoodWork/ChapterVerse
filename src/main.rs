b31c94aef35a934646e3cc2856425392728fd66f
pinghajen <pinghajen@gmail.com>
2024-02-15 23:00:46 -0500

    Started adding a logger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,63 @@
 use helpers::import_bibles::start_bible_import;
 
 use crate::helpers::env_variables::get_env_variable;
-use crate::helpers::print_color::PrintCommand;
 use std::{env, fs};
+use log::{debug, error, info, trace, warn};
+
+use std::time::SystemTime;
+use std::io;
+use humantime;
 
 mod helpers;
 mod scripture;
 
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+    .format(|out, message, record| {
+        out.finish(format_args!(
+            "[{} {} {}] {}",
+            humantime::format_rfc3339_seconds(SystemTime::now()),
+            record.level(),
+            record.target(),
+            message,
+        ))
+    })
+    .chain(
+        fern::Dispatch::new()
+            // by default only accept warn messages
+            .level(log::LevelFilter::Warn)
+            // accept info messages from the current crate too
+            .level_for("chapterverse", log::LevelFilter::Trace)
+            // `io::Stdout`, `io::Stderr` and `io::File` can be directly passed in.
+            .chain(io::stdout()),
+    )
+    .chain(
+        fern::Dispatch::new()
+            // output all messages
+            .level(log::LevelFilter::Trace)
+            .chain(fern::log_file("logs/log.log")?)
+    )
+    .chain(
+        fern::Dispatch::new()
+            .level(log::LevelFilter::Error)
+            .chain(io::stderr()),
+    )
+    .apply()?;
+
+    Ok(())
+}
+
 fn main() {
-    PrintCommand::System.print_message("ChapterVerse", "Jesus is Lord!");
-    PrintCommand::Issue.print_message("Version", env!("CARGO_PKG_VERSION"));
-    PrintCommand::System.print_message("What is the Gospel?", "Gospel means good news! The bad news is we have all sinned and deserve the wrath to come. But Jesus the Messiah died for our sins, was buried, and then raised on the third day, according to the scriptures. He ascended into heaven and right now is seated at the Father's right hand. Jesus said, \"I am the way, and the truth, and the life. No one comes to the Father except through me. The time is fulfilled, and the kingdom of God is at hand; repent and believe in the gospel.\"");
+    setup_logger().unwrap();
+    
+    info!("Jesus is Lord!");
+    info!("Version {}", env!("CARGO_PKG_VERSION"));
+    info!("What is the Gospel? Gospel means good news! The bad news is we have all sinned and
+        deserve the wrath to come. But Jesus the Messiah died for our sins, was buried, and then
+        raised on the third day, according to the scriptures. He ascended into heaven and right now
+        is seated at the Father's right hand. Jesus said, \"I am the way, and the truth, and the
+        life. No one comes to the Father except through me. The time is fulfilled, and the kingdom
+        of God is at hand; repent and believe in the gospel.\"");
 
     let import_bibles_path = get_env_variable("IMPORT_BIBLES_PATH", "bibles");
 
@@ -18,7 +65,7 @@ fn main() {
         .expect("Failed to get current directory")
         .join(import_bibles_path);
 
-    PrintCommand::System.print_message("Bibles Directory", &bibles_directory.to_string_lossy());
+    debug!("Bibles Directory {}", &bibles_directory.to_string_lossy());
     
     let files = fs::read_dir(&bibles_directory).expect("Failed to read directory");
 
@@ -27,15 +74,17 @@ fn main() {
         if entry.path().is_file() && entry.path().extension().and_then(|s| s.to_str()) == Some("csv") {
             match start_bible_import(&entry.path().to_string_lossy()) {
                 Ok(imported_bible) => {
-                    PrintCommand::System.print_message("Bible Imported", &entry.path().display().to_string());
-                    PrintCommand::Info.print_message("Number of verses imported", &imported_bible.len().to_string());
-                    PrintCommand::Info.print_message("2 Timothy 3:16", &imported_bible.get_scripture("55:3:16").0.then(|| imported_bible.get_scripture("55:3:16").1).unwrap_or_else(|| "Verse not found".to_string()));
+                    info!("Bible Imported {}", &entry.path().display().to_string());
+                    debug!("Number of verses imported {}", &imported_bible.len().to_string());
+                    debug!("2 Timothy 3:16: {}", &imported_bible.get_scripture("55:3:16").0.then(||
+                            imported_bible.get_scripture("55:3:16").1).unwrap_or_else(|| "Verse not
+                            found".to_string()));
 
 
 
                 },
                 Err(err) => {
-                    println!("Error running import: {}", err);
+                    error!("Error running import: {}", err);
                 }
             }
         }
