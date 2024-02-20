59cb5ad06b537e1e2a17b97e40e730f271bdcda8
Peter Hammond <peter@everygoodwork.online>
2024-02-20 07:20:05 -0800

    Added Benchmark for get_scripture
    Selected one of the longest verses in the bible to test:  Esther 8:9
    ID:  17:8:9
    AVG Response - 74.900 ns

--- benches/bible_benchmark.rs
+++ benches/bible_benchmark.rs
@@ -1,13 +1,10 @@
+use bible::csv_import::bible_import;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::error::Error;
 use std::hint::black_box;
 use std::{env, fs};
 
-use bible::csv_import::bible_import;
-
-use criterion::{criterion_group, criterion_main, Criterion};
-
 fn bible_import_benchmark(c: &mut Criterion) {
-    //let mut criterion = Criterion::default();
-
     let bibles_directory = env::current_dir()
         .expect("Failed to get current directory")
         .join("bibles");
@@ -25,5 +22,35 @@ fn bible_import_benchmark(c: &mut Criterion) {
         }
     }
 }
-    criterion_group!(benches, bible_import_benchmark);
-    criterion_main!(benches);
+
+fn bible_get_scripture_benchmark(c: &mut Criterion) {
+    let bibles_directory = env::current_dir()
+        .expect("Failed to get current directory")
+        .join("bibles");
+    let files = fs::read_dir(&bibles_directory).expect("Failed to read directory");
+
+    println!("{:?}", bibles_directory);
+    for file in files {
+        let file = file.expect("Failed to read file");
+        let path = file.path();
+        if path.extension().and_then(std::ffi::OsStr::to_str) == Some("csv") {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+
+            let bible = bible_import(path.to_str().unwrap()).expect("Failed to import bible");
+
+            c.bench_function("get_scripture 17:8:9", |b| {
+                b.iter(|| {
+                    let scripture = "17:8:9"; // Longest verse in the bible.
+                    bible.get_scripture(black_box(scripture))
+                })
+            });
+        }
+    }
+}
+
+criterion_group!(
+    benches,
+    bible_import_benchmark,
+    bible_get_scripture_benchmark
+);
+criterion_main!(benches);
