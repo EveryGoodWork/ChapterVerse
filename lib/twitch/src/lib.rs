f76c0804e8f78db4fcfd90b33f07c716aa37ff5b
Peter Hammond <peter@everygoodwork.online>
2024-03-23 12:07:27 -0700

    Initial connection to Twitch
    This uses the justinfan to create a listener account and has successfully connected to the Twitch irc channel.

--- lib/twitch/src/lib.rs
+++ lib/twitch/src/lib.rs
@@ -1,5 +1,105 @@
-pub mod temp {
-    pub fn temp() {
-        // Your code here
+use futures_util::{stream::StreamExt, SinkExt};
+use rand::Rng;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use tokio::net::TcpStream;
+use tokio::sync::{mpsc, Mutex};
+use tokio_tungstenite::{
+    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
+
+pub struct WebSocketState {
+    is_connecting: AtomicBool,
+}
+
+impl WebSocketState {
+    pub fn new() -> Self {
+        WebSocketState {
+            is_connecting: AtomicBool::new(false),
+        }
+    }
+}
+
+pub async fn connect(
+    websocket_state: Arc<Mutex<WebSocketState>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = "ws://irc-ws.chat.twitch.tv:80";
+    println!("Attempting to connect to WebSocket at URL: {}", url);
+
+    {
+        let mut ws_state = websocket_state.lock().await;
+        if ws_state.is_connecting.load(Ordering::SeqCst) {
+            println!("Already attempting to connect. Exiting...");
+            return Ok(());
+        } else {
+            ws_state.is_connecting.store(true, Ordering::SeqCst);
+            println!("Connection attempt marked as in progress.");
+        }
+    }
+
+    let ws_stream = connect_async(url)
+        .await
+        .map_err(|e| {
+            println!("Failed to connect: {:?}", e);
+            e
+        })?
+        .0; // Directly take the WebSocketStream from the result tuple
+
+    let (mut write, mut read) = ws_stream.split();
+    let (tx, mut rx) = mpsc::unbounded_channel();
+
+    // Send a test message
+    let nick_command = format!(
+        "NICK justinfan{}",
+        rand::thread_rng().gen_range(10000..=99999)
+    );
+    println!("Sending NICK command: {}", &nick_command);
+    if let Err(e) = tx.send(Message::Text(nick_command)) {
+        println!("Failed to send NICK command: {:?}", e);
+    } else {
+        println!("NICK command sent successfully.");
+    }
+
+    tokio::spawn(async move {
+        while let Some(message) = rx.recv().await {
+            match write.send(message).await {
+                Ok(_) => println!("Message sent successfully."),
+                Err(e) => println!("Error sending message: {:?}", e),
+            }
+        }
+    });
+
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(Message::Text(text)) => {
+                println!("Received message: {}", text);
+                handle_message(text, &tx).await;
+            }
+            Ok(Message::Ping(ping)) => {
+                println!("Received ping, sending pong.");
+                if let Err(e) = tx.send(Message::Pong(ping)) {
+                    println!("Failed to send PONG response: {:?}", e);
+                }
+            }
+            Ok(_) => (),
+            Err(e) => println!("Error reading message: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+async fn handle_message(message: String, tx: &mpsc::UnboundedSender<Message>) {
+    if message.starts_with("PING") {
+        println!("Handling PING message.");
+        if let Err(e) = tx.send(Message::Text("PONG".into())) {
+            println!("Failed to respond to PING with PONG: {:?}", e);
+        }
+    } else if message.contains("PRIVMSG") {
+        println!("Received PRIVMSG: {}", message);
+    } else if message.contains(":Welcome, GLHF!") {
+        println!("Connected to Twitch");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use bible::csv_import::bible_import;
 use bible::scripture::bible::Bible;
 use helpers::env_variables::get_env_variable;
 use helpers::print_color::PrintCommand;
+use tokio::sync::Mutex;
+use twitch::connect;
+use twitch::{self, WebSocketState};
+
 use lazy_static::lazy_static;
 use std::collections::HashMap;
-use std::io::Write;
 use std::sync::Arc;
-use std::{env, fs, io};
+use std::{env, fs};
 
 mod helpers;
 
@@ -67,46 +70,59 @@ lazy_static! {
     };
 }
 
-fn get_bibles_names() -> String {
-    BIBLES.keys().cloned().collect::<Vec<_>>().join(", ")
-}
+// fn get_bibles_names() -> String {
+//     BIBLES.keys().cloned().collect::<Vec<_>>().join(", ")
+// }
 
-fn get_specific_bible(bible_name: &str) -> Option<Arc<Bible>> {
-    let bibles = Arc::clone(&BIBLES); // Clone the Arc for thread-safe access
-    let lookup_name = bible_name.to_uppercase(); // Convert the lookup name to lowercase
-    bibles.get(&lookup_name).cloned()
-}
-
-fn main() {
+// fn get_specific_bible(bible_name: &str) -> Option<Arc<Bible>> {
+//     let bibles = Arc::clone(&BIBLES); // Clone the Arc for thread-safe access
+//     let lookup_name = bible_name.to_uppercase(); // Convert the lookup name to lowercase
+//     bibles.get(&lookup_name).cloned()
+// }
+#[tokio::main]
+async fn main() {
     PrintCommand::System.print_message("ChapterVerse", "Jesus is Lord!");
     PrintCommand::Issue.print_message("Version", env!("CARGO_PKG_VERSION"));
     PrintCommand::Info.print_message("What is the Gospel?", "Gospel means good news! The bad news is we have all sinned and deserve the wrath to come. But Jesus the Messiah died for our sins, was buried, and then raised on the third day, according to the scriptures. He ascended into heaven and right now is seated at the Father's right hand. Jesus said, \"I am the way, and the truth, and the life. No one comes to the Father except through me. The time is fulfilled, and the kingdom of God is at hand; repent and believe in the gospel.\"");
-
-    //Temp commandline to confirm lookup of scripture is working
-    let mut bible_name = String::new();
-    let mut scripture_reference = String::new();
-
-    print!("Enter Bible version ({}): ", get_bibles_names());
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut bible_name).unwrap();
-    bible_name = bible_name.trim().to_string();
-
-    print!("Enter Scripture reference (2 Tim 3:16): ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut scripture_reference).unwrap();
-    scripture_reference = scripture_reference.trim().to_string();
-
-    println!("{}: {}", bible_name, scripture_reference);
-    if let Some(bible_arc) = get_specific_bible(&bible_name) {
-        let bible: &Bible = &bible_arc;
-        if let Some(verse) = bible.get_scripture(&scripture_reference) {
-            println!("{}: {}", verse.reference, verse.scripture);
-        } else {
-            println!("Verse not found");
-        }
-    } else {
-        println!("Bible version not found: {}", bible_name);
+    for (bible_name, bible_arc) in BIBLES.iter() {
+        let bible: &Bible = &*bible_arc; // Dereference the Arc and immediately borrow the result
+        let message = match bible.get_scripture("2 Timothy 3:16") {
+            Some(verse) => format!("{}", verse.scripture),
+            None => "Verse not found".to_string(),
+        };
+        PrintCommand::Info.print_message(&format!("{}, 2 Timothy 3:16", bible_name), &message);
     }
+
+    let websocket_state = Arc::new(Mutex::new(WebSocketState::new()));
+    if let Err(e) = connect(websocket_state).await {
+        eprintln!("Error during connection: {}", e);
+    }
+
+    // //Temp commandline to confirm lookup of scripture is working
+    // let mut bible_name = String::new();
+    // let mut scripture_reference = String::new();
+
+    // print!("Enter Bible version ({}): ", get_bibles_names());
+    // io::stdout().flush().unwrap();
+    // io::stdin().read_line(&mut bible_name).unwrap();
+    // bible_name = bible_name.trim().to_string();
+
+    // print!("Enter Scripture reference (2 Tim 3:16): ");
+    // io::stdout().flush().unwrap();
+    // io::stdin().read_line(&mut scripture_reference).unwrap();
+    // scripture_reference = scripture_reference.trim().to_string();
+
+    // println!("{}: {}", bible_name, scripture_reference);
+    // if let Some(bible_arc) = get_specific_bible(&bible_name) {
+    //     let bible: &Bible = &bible_arc;
+    //     if let Some(verse) = bible.get_scripture(&scripture_reference) {
+    //         println!("{}: {}", verse.reference, verse.scripture);
+    //     } else {
+    //         println!("Verse not found");
+    //     }
+    // } else {
+    //     println!("Bible version not found: {}", bible_name);
+    // }
 }
 
 #[cfg(test)]
