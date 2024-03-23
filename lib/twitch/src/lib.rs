a5387ebf76d425275f9b69fb797314d6580d14e6
Peter Hammond <peter@everygoodwork.online>
2024-03-23 15:32:14 -0700

    Refactoring to help clean up operations
    Incremental commit.

--- lib/twitch/src/lib.rs
+++ lib/twitch/src/lib.rs
@@ -5,11 +5,8 @@ use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
 };
-use tokio::net::TcpStream;
 use tokio::sync::{mpsc, Mutex};
-use tokio_tungstenite::{
-    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
-};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 pub struct WebSocketClient {
     pub tx: Mutex<Option<mpsc::UnboundedSender<Message>>>,
@@ -27,9 +24,8 @@ impl WebSocketClient {
     pub async fn connect(&self) -> Result<(), Box<dyn std::error::Error>> {
         let url = "ws://irc-ws.chat.twitch.tv:80";
         println!("Attempting to connect to WebSocket at URL: {}", url);
-
         {
-            let mut ws_state = self.websocket_state.lock().await;
+            let ws_state = self.websocket_state.lock().await;
             if ws_state.is_connecting.load(Ordering::SeqCst) {
                 println!("Already attempting to connect. Exiting...");
                 return Ok(());
@@ -52,33 +48,19 @@ impl WebSocketClient {
 
         *self.tx.lock().await = Some(tx.clone());
 
-        // Add the justinfan message sending logic here
-        let nick_command = format!(
+        let nick_message = format!(
             "NICK justinfan{}",
             rand::thread_rng().gen_range(10000..=99999)
         );
-        println!("Sending NICK command: {}", &nick_command);
-        if let Err(e) = write.send(Message::Text(nick_command)).await {
-            println!("Failed to send NICK command: {:?}", e);
-        } else {
-            println!("NICK command sent successfully.");
-        }
 
-        let _ = self.send_message("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership twitch.tv/subscriptions twitch.tv/bits twitch.tv/badges").await;
-        let _ = self.send_message("JOIN #chapterverse").await;
-
-        // println!("JoinChannel command: {}", &nick_command);
-        // if let Err(e) = write.send(Message::Text(nick_command)).await {
-        //     println!("Failed to send NICK command: {:?}", e);
-        // } else {
-        //     println!("NICK command sent successfully.");
-        // }
+        self.send_message(&nick_message).await?;
+        self.send_message("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership twitch.tv/subscriptions twitch.tv/bits twitch.tv/badges").await?;
+        self.send_message("JOIN #chapterverse").await?;
 
         tokio::spawn(async move {
             while let Some(message) = rx.recv().await {
-                match write.send(message).await {
-                    Ok(_) => println!("Message sent successfully."),
-                    Err(e) => println!("Error sending message: {:?}", e),
+                if let Err(e) = write.send(message).await {
+                    println!("Error sending message: {:?}", e);
                 }
             }
         });
@@ -90,24 +72,16 @@ impl WebSocketClient {
     async fn listen_to_incoming_messages(
         &self,
         read: impl StreamExt<Item = Result<Message, tokio_tungstenite::tungstenite::Error>>,
-        tx: mpsc::UnboundedSender<Message>,
+        _tx: mpsc::UnboundedSender<Message>,
     ) {
         pin_mut!(read); // This pins the read stream to the stack
         while let Some(message) = read.next().await {
             match message {
                 Ok(Message::Text(text)) => {
-                    // println!("Received Raw Message: {}", text);
                     self.handle_message(text).await;
                 }
-                // Ok(Message::Ping(ping)) => {
-                //     //it doesn't appear that this works.
-                //     println!("Received ping, sending pong.");
-                //     if let Err(e) = self.send_message(Message::Pong(ping)).await {
-                //         println!("Failed to send PONG response: {:?}", e);
-                //     }
-                // }
-                Ok(_) => (),
                 Err(e) => println!("Error reading message: {:?}", e),
+                _ => {} // Ignore other cases
             }
         }
     }
@@ -124,15 +98,6 @@ impl WebSocketClient {
             println!("Connected to Twitch");
         }
     }
-
-    // pub async fn send_message(&self, message: Message) -> Result<(), &'static str> {
-    //     let tx_lock = self.tx.lock().await;
-    //     if let Some(tx) = &*tx_lock {
-    //         tx.send(message).map_err(|_| "Failed to send message")
-    //     } else {
-    //         Err("Connection not initialized")
-    //     }
-    // }
 
     pub async fn send_message(&self, message_str: &str) -> Result<(), &'static str> {
         let message = Message::Text(message_str.to_string());
